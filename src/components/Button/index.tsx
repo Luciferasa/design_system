59bbee298f5b08b52c1f4aae432047aa6d231638
Natalia Reshetova <natalie.reshetova@yandex.ru>
2023-08-03 17:09:23 +0400

    initial commit

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,26 +1,5 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 
-import './style.scss';
-
-const Button = () => {
-  const buttonRef = useRef<HTMLButtonElement | null>(null);
-
-  useEffect(() => {
-    const initialX = Math.random() * 100;
-    const initialY = Math.random() * 100;
-
-    buttonRef.current!.style.top = `${initialX}%`;
-    buttonRef.current!.style.right = `${initialY}%`;
-  }, []);
-
-  const runAway = (e: React.MouseEvent) => {
-    const newX = Math.random() * 100;
-    const newY = Math.random() * 100;
-
-    buttonRef.current!.style.top = `${newY}%`;
-    buttonRef.current!.style.right = `${newX}%`;
-  };
-  return <button ref={buttonRef} className="button" onMouseOver={runAway}>ololo</button>;
-};
+const Button = () => <div>ololo</div>;
 
 export default Button;
