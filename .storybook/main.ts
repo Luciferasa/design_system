8c8edb530772ae60a522d2b85d6282816b0dfd85
Natalia Reshetova <natalie.reshetova@yandex.ru>
2023-08-09 22:58:31 +0400

    fix storybook settings

---
+++ .storybook/main.ts
@@ -0,0 +1,26 @@
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        sass: {
+          // Require your Sass preprocessor here
+          implementation: require('sass'),
+        },
+      },
+    },
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
---
+++ src/stories/Button.stories.js
@@ -0,0 +1,10 @@
+import Button from '../components/Button/index.tsx';
+
+export default {
+  title: 'Button',
+  component: Button,
+};
+
+const Template = () => <Button />;
+
+export const Default = Template.bind({});
