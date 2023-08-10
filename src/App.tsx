76bdd137f61cbe7f6e73eef3bae272ed0e09dfd1
Natalia Reshetova <natalie.reshetova@yandex.ru>
2023-08-10 11:28:49 +0400

    fix

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Button from './components/Button/index.tsx';
+import PhoneSelector from './components/PhoneSelector';
+
+const App = () => (
+  <div className="App">
+    <Button />
+    <PhoneSelector />
+  </div>
+);
+
+export default App;
